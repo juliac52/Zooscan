1929be2f877ae22cd1eda5fe17bcbd586b13964b
Julia Cox <juliacox@Julias-MacBook-Pro-3.local>
2020-06-19 14:54:39 -0400

    Test

--- Functions Accounting for Splits.R
+++ Functions Accounting for Splits.R
@@ -1,4 +1,4 @@
-#function for reading in >5000um files and converting pixels to micrometers (um)
+#function for reading in >5000um and 1000-5000um files and converting pixels to micrometers (um)
 readZooscan<-function(filename){
   library(reader)
   #Removing the header: 
@@ -22,17 +22,17 @@ readZooscan<-function(filename){
   cat('raw string data at that line is: ', allLines[PixelSizeLine])
   #This convertion from string to numeric type is what we want to do math on
   #sub() function strips off all the character stuff before the useful number
-  #and replaces it with nothing (shown by ""), decimalNumeric is the value we 
+  #and replaces it with nothing (shown by ""), decimalNumericPix is the value we 
   #use for unit convertions
-  decimalNumeric <- as.numeric(sub("PixSize=|","", allLines[PixelSizeLine]))
+  decimalNumericPix <- as.numeric(sub("PixSize=|","", allLines[PixelSizeLine]))
   #get numeric mm/pix value
-  cat(decimalNumeric)
+  cat(decimalNumericPix)
   
   #read in data table removing everything before headerEnd
   zooscandata <- read.table(file=filename, sep = ';', header = T, row.names=1, skip = headerEnd)
   
   #convert from mm/pix to um/pix
-  umpix <- decimalNumeric * 1000
+  umpix <- decimalNumericPix * 1000
   print(umpix)
   
   #multiply Min and Max by 10.6 um/pix (umpix) to get size in um
@@ -43,20 +43,62 @@ readZooscan<-function(filename){
   
   #Search for and extract the volume of water filtered (m^3) 
   VolLine <- grep("Vol=", allLines)
-  #print the line numner where volume filtered is given in m^3 
+  #print the line number where volume filtered is given in m^3 
   cat('line number of Vol Filtered = ', VolLine)
   #print the contents of the VolLine in allLines
   cat('raw string data at that line is: ', allLines[VolLine])
   #This convertion from string to numeric type is what we want to do math on
   #sub() function strips off all the character stuff before the useful number
-  #and replaces it with nothing (shown by ""), decimalNumeric2 is the value we 
+  #and replaces it with nothing (shown by ""), decimalNumericVol is the value we 
   #use for unit convertions
   decimalNumericVol <- as.numeric(sub("Vol=|","", allLines[VolLine]))
-  #get numeric mm/pix value
+  #get numeric Vol value
   cat(decimalNumericVol)
-  #calculate zooplankton area per 100m^3, accounting for 1/4 split  
+  
+  #Search for and extract the split done directly before scanning 
+  SplitLine <- grep("SubPart=", allLines)
+  #print the line number where split fraction (1=1/1, 2=1/2, 4=1/4, etc.)
+  cat('line number of Split = ', SplitLine)
+  #print the contents of the VolLine in allLines
+  cat('raw string data at that line is: ', allLines[SplitLine])
+  #This convertion from string to numeric type is what we want to do math on
+  #sub() function strips off all the character stuff before the useful number
+  #and replaces it with nothing (shown by ""), decimalNumericSplit is the value we 
+  #use for unit convertions. 
+  #For some reason, this is printing the number 2X i.e. 1 would print as
+  #11 and 2 would print as 22. The math works correctly (11*5 = 5, 22*5 = 10) 
+  #but this may be an issue later. This is probably just an error with printing, 
+  #and not with the actual numeric value 
+  decimalNumericSplit <- as.numeric(sub("SubPart=|","", allLines[SplitLine]))
+  #get numeric mm/pix value
+  cat(decimalNumericSplit)
+  
+  #calculate zooplankton area accounting for 1/4 split  
   zooscandata$Area.um2x4 <- zooscandata$Area.um2 * 4
-  zooscandata$Area.um2per100m3 <- (zooscandata$Area.um2x4 * 100)/decimalNumericVol
+  #calculate zooplankton area per 100m^3, accounting for 1/4 split and additional splits 
+  zooscandata$Area.um2per100m3 <- 
+    (zooscandata$Area.um2x4 * decimalNumericSplit * 100)/decimalNumericVol
+  
+  #Add two columns showing max and min depth of net tow 
+  #Search for and extract the two depths (m)
+  MaxDepthLine <- grep("Zmax=", allLines)
+  MinDepthLine <- grep("Zmin=", allLines)
+  #print the line numbers where depths are
+  cat('line number of Max Depth = ', MaxDepthLine)
+  cat('line number of Min Depth = ', MinDepthLine)
+  #print the contents of the objects in allLines
+  cat('raw string data at that line is: ', allLines[MaxDepthLine])
+  cat('raw string data at that line is: ', allLines[MinDepthLine])
+  #Convert to decimal numeric 
+  decimalNumericMaxDepth <- as.numeric(sub("Zmax=|","", allLines[MaxDepthLine]))
+  decimalNumericMinDepth <- as.numeric(sub("Zmin=|","", allLines[MinDepthLine]))
+  #get numeric mm/pix value
+  cat(decimalNumericMaxDepth)
+  cat(decimalNumericMinDepth)
+  
+  zooscandata$Max_Depth <- decimalNumericMaxDepth
+  zooscandata$Min_Depth <- decimalNumericMinDepth
   
   return(zooscandata)
 }
+
