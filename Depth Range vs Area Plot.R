014df7a5d1f52bdef6df1f9b520198afaddedc97
Katie Redmond <KatieRedmond@Katies-MacBook-Pro.local>
2020-06-26 10:37:25 -0400

    Plz work

--- Depth Range vs Area Plot.R
+++ Depth Range vs Area Plot.R
@@ -1,7 +1,11 @@
+getwd()
+
+setwd("C:/Users/KatieRedmond/Desktop/Zooscan Git KR")
+
 #code to combine matrices, provide depth range information, and make a plot to 
 #reflect the relationship between area and depth based on “merging data.R”
 #read in all 11 333um samples using readZooscan333 function 
-moc8_net1_333<-readZooscan333("hb1907_moc8_net1_333_1_tot_1_dat1 (1).pid","hb1907_moc8_net1_333_2_tot_1_dat1.pid")
+moc8_net1_333<-readZooscan333("hb1907_moc8_net1_333_1_tot_1_dat1.pid","hb1907_moc8_net1_333_2_tot_1_dat1.pid")
 moc8_net2_333<-readZooscan333("hb1907_moc8_net2_333_1_tot_1_dat1.pid","hb1907_moc8_net2_333_2_tot_1_dat1.pid")
 moc8_net5_333<-readZooscan333("hb1907_moc8_net5_333_1_tot_1_dat1.pid","hb1907_moc8_net5_333_2_tot_1_dat1.pid")
 moc12_net0_333<-readZooscan333("hb1907_moc12_net0_333_1_tot_1_dat1.pid","hb1907_moc12_net0_333_2_tot_1_dat1.pid")
@@ -12,6 +16,23 @@ moc11_net0_333<-readZooscan333("hb1907_moc11_net0_333_1_tot_1_dat1.pid","hb1907_
 moc11_net3_333<-readZooscan333("hb1907_moc11_net3_333_1_tot_1_dat1.pid","hb1907_moc11_net3_333_2_tot_1_dat1.pid")
 moc11_net5_333<-readZooscan333("hb1907_moc11_net5_333_1_tot_1_dat1.pid","hb1907_moc11_net5_333_2_tot_1_dat1.pid")
 moc10_net3_333<-readZooscan333("hb1907_moc10_net3_333_1_tot_1_dat1.pid","hb1907_moc10_net3_333_2_tot_1_dat1.pid")
+
+#code to combine matrices, provide depth range information, and make a plot to 
+#reflect the relationship between area and depth based on “merging data.R”
+#read in all 11 1000 um samples using readZooscan function 
+
+moc8_net1_1000<-readZooscan("hb1907_moc8_net1_1000_1_tot_1_dat1.pid")
+moc8_net5_1000<-readZooscan(
+moc12_net0_1000<-readZooscan(
+moc12_net2_1000<-readZooscan(
+moc12_net5_1000<-readZooscan(
+moc12_net6_1000<-readZooscan(
+moc11_net0_1000<-readZooscan(
+moc11_net3_1000<-readZooscan(
+moc11_net5_1000<-readZooscan()
+moc10_net3_1000<-readZooscan("hb1907_moc10_net3_1000_1_tot_1_dat1.pid")
+
+
 #combine all 333um samples into one dataframe called moc_333 
 moc_333 <- rbind(moc8_net1_333,moc8_net2_333,moc8_net5_333,moc12_net0_333,moc12_net2_333,
                  moc12_net5_333,moc12_net6_333,moc11_net0_333,moc11_net3_333,moc11_net5_333,
