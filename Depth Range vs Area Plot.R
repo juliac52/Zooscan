f37f5ab840f80ed23d0779a1027341395dfeb59e
Katie Redmond <KatieRedmond@Katies-MacBook-Pro.local>
2020-06-26 12:10:28 -0400

    updates

--- Depth Range vs Area Plot.R
+++ Depth Range vs Area Plot.R
@@ -69,4 +69,38 @@ plot<-ggplot(moc_1000, aes(x= Depth_Range, y=Area.um2per100m3, fill = Depth_Rang
   geom_bar(stat = "identity") + coord_flip() + 
   scale_x_discrete(limits = rev(levels(moc_1000$Depth_Range)))
 plot
-#####
+###############################
+
+#code to combine matrices, provide depth range information, and make a plot to 
+#reflect the relationship between area and depth based on “merging data.R”
+#read in all 11 1000 um samples using readZooscan function 
+
+moc8_net1_5000<-readZooscan("hb1907_moc8_net1_5000_1_tot_1_dat1.pid")
+moc8_net2_5000<-readZooscan("hb1907_moc8_net2_5000_1_tot_1_dat1.pid")
+moc12_net0_5000<-readZooscan("hb1907_moc12_net0_5000_1_tot_1_dat1.pid")
+moc12_net2_5000<-readZooscan("hb1907_moc12_net2_5000_1_tot_1_dat1.pid")
+moc12_net5_5000<-readZooscan("hb1907_moc12_net5_5000_1_tot_1_dat1.pid")
+moc12_net6_5000<-readZooscan("hb1907_moc12_net6_5000_1_tot_1_dat1.pid")
+moc11_net0_5000<-readZooscan("hb1907_moc11_net0_5000_1_tot_1_dat1.pid")
+moc11_net3_5000<-readZooscan("hb1907_moc11_net3_5000_1_tot_1_dat1.pid")
+moc11_net5_5000<-readZooscan("hb1907_moc11_net5_5000_1_tot_1_dat1.pid")
+moc10_net3_5000<-readZooscan("hb1907_moc10_net3_5000_1_tot_1_dat1.pid")
+
+
+#combine all 1000um samples into one dataframe called moc_1000
+moc_5000 <- rbind(moc8_net1_5000,moc8_net2_5000,moc12_net0_5000,moc12_net2_5000,
+                  moc12_net5_5000,moc12_net6_5000,moc11_net0_5000,moc11_net3_5000,moc11_net5_5000,
+                  moc10_net3_5000)
+#make new columns in moc_1000 for max and min depths as character data 
+moc_5000$Max_Depth_Char <- as.character(moc_5000$Max_Depth)
+moc_5000$Min_Depth_Char <- as.character(moc_5000$Min_Depth)
+#combine the new character depth columns into one depth range column, separated by a "-" 
+moc_5000$Depth_Range = paste(moc_5000$Min_Depth_Char, moc_5000$Max_Depth_Char, sep="-")
+#reorder the depth column (Depth_Range) in relation to another numeric
+#column (Max_Depth)
+moc_5000$Depth_Range = with(moc_5000, reorder(Depth_Range,Max_Depth))
+#plot, with coordinate flip and reversal of x axis (limits = rev)
+pl<-ggplot(moc_5000, aes(x= Depth_Range, y=Area.um2per100m3, fill = Depth_Range)) + 
+  geom_bar(stat = "identity") + coord_flip() + 
+  scale_x_discrete(limits = rev(levels(moc_5000$Depth_Range)))
+pl
